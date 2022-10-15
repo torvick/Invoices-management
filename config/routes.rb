38ccfe42da9d795675b2867e61a31fac6fad8f2b
Torvick <vh.urquides@gmail.com>
2022-10-15 03:21:17 -0500

    Added Authentication Configuration
    added JWT authentication method for API calls
    
    gem design
    gem devise-jwt

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,11 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  devise_for :users, controllers: { sessions: 'api/v1/sessions', registrations: 'api/v1/registrations'}
+
+  namespace :api do
+    namespace :v1 do
+
+    end
+  end
 end
